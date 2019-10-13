5e88e885ec4e0d37381481f332a8b5ca86285a64
Rohit Raj <rohit15079@gmail.com>
2019-10-13 22:39:13 +0530

    Add directory filter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fetchData import fetchPatchList
 from tableColumnIndex import *
 from filterDelegates import filterDelegate
 from filterSubmitters import filterSubmitter
+from filterDirectories import filterDirectory
 
 patchList=fetchPatchList()
 optionalPatchList=[]
@@ -9,6 +10,9 @@ mandatoryPatchList, optionalPatches=filterDelegate(patchList)
 for patch in optionalPatches:
     optionalPatchList.append(patch)
 mandatoryPatchList, optionalPatches=filterSubmitter(mandatoryPatchList)
+for patch in optionalPatches:
+    optionalPatchList.append(patch)
+mandatoryPatchList, optionalPatches=filterDirectory(mandatoryPatchList)
 for patch in optionalPatches:
     optionalPatchList.append(patch)
 print(mandatoryPatchList)
